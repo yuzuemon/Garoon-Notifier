337172d766d6317fbbfa619c6dc12ada09f0d28c
yuzuemon <killerqueen64@gmail.com>
2012-02-10 20:29:30 +0900

    add popup function[fix] & add notification function[halfway]

--- background.js
+++ background.js
@@ -1,14 +1,16 @@
 var URL = "http://portal/cgi-bin/cbgrn/grn.cgi/notification/pending_list";
 var LIMIT = 4;
 var LIMIT_COUNT = Math.pow(10, LIMIT);
+var BADGECOUNT = 0;
 
+// badge
 function check(){
   var xhr = new XMLHttpRequest();
   xhr.open('Get', URL, true);
   xhr.onload = function(){
     var res = xhr.responseText;
     var count = parseInt(res.match(/[0-9]+件/)[0].replace('件', ''), 10);
-    if(count > 0){
+    if(count >= 0){
       update(String(count));
       setTimeout(check, 60 * 1000);
     } else {
@@ -26,6 +28,10 @@ function update(count){
     count = notation(parseInt(count,10), LIMIT+1);
   }
   chrome.browserAction.setBadgeText({text: count});
+  if (BADGECOUNT != count){
+    notification(BADGECOUNT - count);
+  }
+  BADGECOUNT = count;
 }
 
 function notation(count, i){
@@ -37,8 +43,20 @@ function notation(count, i){
   }
 }
 
+// webkitNotification
+function notification(count){
+  // xhrでcountの数だけ新着取得
+  // その数だけ出力
+  webkitNotifications.createNotification('logo.ico', 'title', 'ほげ').show();
+}
+
 check();
 
-window.setInterval(function(){
+setInterval(function(){
   check();
 }, 60 * 1000);
+
+
+// notification
+webkitNotifications.createNotification('logo.ico', 'title', 'ほげ').show();
+
--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@ var w = window;
 var d = w.document;
 
 function write(html){
-  d.querySelector('#wrapper').innerHTML=html;
+  d.querySelector('#wrapper').innerHTML = html;
 }
 
 function popup(){
@@ -11,8 +11,18 @@ function popup(){
   xhr.open('Get', URL, true);
   xhr.onload = function(){
     var res = xhr.responseText;
-    var unreadTable = res.match(/<table class="list_column">[\s\S]*?<\/table>/);
+    var unreadTable = res.match(/<table class="list_column">[\s\S]*?<\/table>/)[0];
+    unreadTable = unreadTable.replace(/<script .*?>/g, '');
+    unreadTable = unreadTable.replace(/<button .*?>/g, '');
+    unreadTable = unreadTable.replace(/<img .*?>/g, '');
+    unreadTable = unreadTable.replace(/<a class="" href="\/cgi-bin/g, '<a target="_blank" href="http://portal/cgi-bin');
+    unreadTable = unreadTable.replace(/javascript:popupWin\('/g, 'javascript:window.open(\'http://portal');
     write(unreadTable);
+
+    var links = d.querySelector('a');
+    for (var i = 0, len = links.length; len > i; i++){
+      links[i].target = "_blank";
+    }
   };
   xhr.onerror = function(){
     write('<a href=' + URL + '>');
